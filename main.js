623444bea2f676cd7f9c2a2c8022298a1adacca8
AnnaGruber263 <anna.gruber@uibk.ac.at>
2024-05-16 16:27:37 +0200

    Wetter Icons für die nächsten 24 Stunden

--- main.js
+++ main.js
@@ -40,9 +40,8 @@ async function showForecast(url) {
     L.geoJSON(jsondata, {
         pointToLayer: function (feature, latlng) {
             let details = feature.properties.timeseries[0].data.instant.details;
-            console.log(details);
             let time = new Date(feature.properties.timeseries[0].time);
-            console.log(time);
+
             let content = `
             <h4>Wettervorhersage für ${time.toLocaleString()}</h4>
             <ul>
@@ -54,6 +53,12 @@ async function showForecast(url) {
                 <li>Windgeschwindigkeit (km/h): ${Math.round(details.wind_speed * 3.6)}</li>
             </ul>
             ` ;
+
+            for (let i = 0; i <= 24; i += 3) {
+                let symbol = feature.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+                content += `<img src="icons/${symbol}.svg" alt="${symbol}" style="width:32px">`;
+            }
+
             L.popup(latlng, {
                 content: content
             }).openOn(themaLayer.forecast);
